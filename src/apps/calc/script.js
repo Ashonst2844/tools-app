98966ca1c3e10f640fe0869d4631ec7245a3945a
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-02-18 00:39:04 -0800

    create skelaton of calculator app

---
+++ src/apps/calc/script.js
@@ -0,0 +1,20 @@
+var display = document.querySelector("#display");
+
+function appendNumber(number) {
+
+}
+function appendOperator(opr) {
+    
+}
+function clearAll() {
+    
+}
+function clearEntry() {
+    
+}
+function del() {
+    
+}
+function calculate() {
+    
+}
