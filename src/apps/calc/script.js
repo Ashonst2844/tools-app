086801dc44dab2ed00adba843fa40d4dae5e0a0f
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-02-20 16:27:06 +0700

    create app calculator v1.0

--- src/apps/calc/script.js
+++ src/apps/calc/script.js
@@ -1,20 +1,30 @@
 var display = document.querySelector("#display");
 
-function appendNumber(number) {
+var onOperation = "";
 
+function appendNumber(number) {
+    onOperation += number;
+    display.textContent = onOperation;
 }
 function appendOperator(opr) {
-    
+    onOperation += opr;
+    display.textContent = onOperation;
 }
 function clearAll() {
-    
-}
-function clearEntry() {
-    
+    onOperation = "";
+    display.textContent = onOperation;
 }
 function del() {
-    
+    onOperation = onOperation.slice(0, -1);
+    display.textContent = onOperation;
 }
 function calculate() {
-    
+    try {
+        var result = eval(onOperation);
+        display.textContent = result;
+        onOperation = result
+    } catch (error) {
+        alert("Error input!");
+        location.reload();
+    }
 }
