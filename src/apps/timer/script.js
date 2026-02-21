57bf12ff9da73add77fffc39bc96a500c8facf60
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-02-21 23:30:40 +0700

    continue timer app v1.0

--- src/apps/timer/script.js
+++ src/apps/timer/script.js
@@ -1,12 +1,55 @@
-const hDis = document.querySelector("#hour");
-const mDis = document.querySelector("#minute");
-const sDis = document.querySelector("#second");
+let hDis = document.querySelector("#hour");
+let mDis = document.querySelector("#minute");
+let sDis = document.querySelector("#second");
+
+const t = 60;
+
+let counting = null;
 
 function reset() {
-    hDis.value = 0;
-    mDis.value = 0;
-    sDis.value = 0;
+    location.reload();
 }
-function playStop() {
-    console.log(`H = ${hDis.value}, M = ${mDis.value}, S = ${sDis.value}`)
+
+function playStop(timer) {
+    var totalTime;
+
+    if (counting !== null) {
+        clearInterval(counting);
+        counting=null;
+        return;
+    }
+
+    var h = parseInt(hDis.value*t*t) || 0;
+    var m = parseInt(mDis.value*t) || 0;
+    var s = parseInt(sDis.value) || 0;
+
+    if (totalTime==0) {
+        totalTime = h+m+s;
+    } else {
+        totalTime = timer;
+    }
+    console.log(totalTime)
+
+    if (!totalTime) {
+        alert("Enter You Time")
+    } else { 
+        counting = setInterval(function () {
+            if (totalTime <= 0) {
+                alert("TimeOut");
+                clearInterval(counting)
+            } else {
+
+                totalTime--;
+                hDis.value = Math.floor(totalTime/(t*t));
+                console.log(hDis.value);
+
+                mDis.value = Math.floor((totalTime-(hDis.value*(t*t)))/t);
+                console.log(mDis.value)
+
+                sDis.value = totalTime%t
+                console.log(sDis.value)
+                console.log(totalTime)
+            }
+        }, 1000)
+    }
 }
